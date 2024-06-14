1a641ffeb5bee2891f434781fe15eb1247ea8ce9
Ishan Lahiru <ishanlahiru2012@gmail.com>
2024-06-14 15:35:24 +0530

    Completed and tuned

--- teacher-app-front/src/app/view/teacher-add/teacher-add.component.ts
+++ teacher-app-front/src/app/view/teacher-add/teacher-add.component.ts
@@ -9,9 +9,9 @@ import {TeacherReqDto} from "../../dto/teacher-req-dto";
         <h2 class="text-2xl font-bold font-serif">Add Teacher Here</h2>
         <div class="w-full  flex space-x-2">
           <input class="px-3 py-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
-                 [(ngModel)]="teacherName" placeholder="Enter teacher name">
+                 [(ngModel)]="teacherName" placeholder="Enter Teacher Name">
           <button (click)="addTeacher()"
-                  class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none">
+                  class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none">
             Add Teacher
           </button>
         </div>
