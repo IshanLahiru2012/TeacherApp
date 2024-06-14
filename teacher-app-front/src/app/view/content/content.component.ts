fae44112edb6e02f1c749813308439a178f32871
Ishan Lahiru <ishanlahiru2012@gmail.com>
2024-06-14 19:06:27 +0530

    Responsive added

--- teacher-app-front/src/app/view/content/content.component.ts
+++ teacher-app-front/src/app/view/content/content.component.ts
@@ -5,7 +5,7 @@ import {TeacherListComponent} from "../teacher-list/teacher-list.component";
 @Component({
   selector: 'app-content',
   template: `
-    <div class=" flex flex-col space-y-6">
+    <div class=" flex flex-col space-y-6 w-screen px-10">
         <app-teacher-add ></app-teacher-add>
         <app-teacher-list></app-teacher-list>
     </div>
--- teacher-app-front/src/app/view/teacher-list/teacher-list.component.ts
+++ teacher-app-front/src/app/view/teacher-list/teacher-list.component.ts
@@ -7,33 +7,34 @@ import {Subscription} from "rxjs";
   selector: 'app-teacher-list',
   template: `
 
-    <div class="mx-auto">
+    <div class="">
       <header class="p-4 font-bold text-amber-700 text-4xl font-serif ">
         <p>Teachers List</p>
       </header>
-      <table class="min-w-full divide-y divide-gray-200">
+      <div class="table-container">
+        <table class="min-w-full divide-y divide-gray-200 ">
           <thead class="bg-gray-50">
           <tr>
-            <th scope="col" class="table-header">
+            <th scope="col" class="table-header id-column">
               Id
             </th>
-            <th scope="col" class="table-header">
+            <th scope="col" class="table-header ">
               Teacher Name
             </th>
           </tr>
           </thead>
           <tbody class="bg-white divide-y divide-gray-200">
-            <ng-container *ngIf="pagedTeachers.length > 0; else noData">
-              <tr *ngFor="let teacher of pagedTeachers; let i = index">
-                <td class="px-6 py-4  bg-sky-400">
-                  {{ teacher.id }}
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  {{ teacher.name }}
-                </td>
-              </tr>
-            </ng-container>
-            <ng-template #noData>
+          <ng-container *ngIf="pagedTeachers.length > 0; else noData">
+            <tr *ngFor="let teacher of pagedTeachers; let i = index">
+              <td class="px-6 py-4  bg-sky-400 id-column">
+                {{ teacher.id }}
+              </td>
+              <td class="px-6 py-4 ">
+                {{ teacher.name }}
+              </td>
+            </tr>
+          </ng-container>
+          <ng-template #noData>
             <tr>
               <td colspan="3" class="px-6 py-4 text-center text-gray-500">No data available</td>
             </tr>
@@ -41,6 +42,8 @@ import {Subscription} from "rxjs";
           </tbody>
 
         </table>
+      </div>
+
 
       <div class="mt-4 flex justify-end">
         <nav class="relative z-0 inline-flex shadow-sm -space-x-px" aria-label="Pagination">
